67356d48b6d82fefaeaa2572935d2a4c623ca200
itspoonam04 <sonampoonam010@gmail.com>
2026-08-04 22:32:30 +0530

    Test homepage

--- homepage/views.py
+++ homepage/views.py
@@ -27,22 +27,27 @@
 
 #     return render(request, "homepage/index.html", context)
 
+# from django.shortcuts import render
+# from categories.models import Category
+# from .models import HeroSlide, FestivalBanner, Testimonial
+
+# def index(request):
+#     hero_slides = HeroSlide.objects.filter(is_active=True)
+#     festival_banners = FestivalBanner.objects.filter(is_active=True)
+#     testimonials = Testimonial.objects.filter(is_active=True)
+#     featured_categories = Category.objects.filter(
+#         is_active=True,
+#         parent=None
+#     )[:8]
+
+#     return render(request, "homepage/index.html", {
+#         "hero_slides": hero_slides,
+#         "festival_banners": festival_banners,
+#         "testimonials": testimonials,
+#         "featured_categories": featured_categories,
+#     })
+
 from django.shortcuts import render
-from categories.models import Category
-from .models import HeroSlide, FestivalBanner, Testimonial
 
 def index(request):
-    hero_slides = HeroSlide.objects.filter(is_active=True)
-    festival_banners = FestivalBanner.objects.filter(is_active=True)
-    testimonials = Testimonial.objects.filter(is_active=True)
-    featured_categories = Category.objects.filter(
-        is_active=True,
-        parent=None
-    )[:8]
-
-    return render(request, "homepage/index.html", {
-        "hero_slides": hero_slides,
-        "festival_banners": festival_banners,
-        "testimonials": testimonials,
-        "featured_categories": featured_categories,
-    })
+    return render(request, "homepage/index.html")
